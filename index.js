3ff74d6029c49493e92e1981c4d087d2a47ba3b5
JordyRein <JordyRein>
2025-05-20 22:02:27 +0200

    forget these things

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-document.body.innerHTML += "Funi Stuff Happens Here";
+import MainPage from './src/mainPage.js';
+
+const app = document.querySelector('app');
+const page = await MainPage();
+
+app.appendChild(page);
 
