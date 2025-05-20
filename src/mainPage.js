796137d8d34071b2c0403768bbd84a01d1d2c2d1
JordyRein <JordyRein>
2025-05-20 22:07:51 +0200

    path wong?

--- src/mainPage.js
+++ src/mainPage.js
@@ -1,5 +1,5 @@
 export default async function MainPage(){
-  const res = await fetch('./mainPage.html');
+  const res = await fetch('./src/mainPage.html');
   const html = await res.text();
 
   const tmpl = document.createElement('template');
