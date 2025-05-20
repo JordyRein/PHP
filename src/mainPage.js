7fcfd13f5cdba2ea5bd308550482305cc7fc3a1d
JordyRein <JordyRein>
2025-05-20 22:31:22 +0200

    debug

--- src/mainPage.js
+++ src/mainPage.js
@@ -1,9 +1,12 @@
 export default async function MainPage(){
   const res = await fetch('./src/mainPage.html');
+  console.log(res);
   const html = await res.text();
+  console.log(html);
 
   const tmpl = document.createElement('template');
   tmpl.innerHTML = html.trim();
+  console.log(tmpl);
 
   return tmpl.content.cloneNode(true);
 }
