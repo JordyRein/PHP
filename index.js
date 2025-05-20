594c26373b300e51120aef97e516476d20c497af
JordyRein <JordyRein>
2025-05-20 22:16:30 +0200

    testing stuff agen

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import MainPage from './src/mainPage.js';
+import './components/banner/banner.js';
+import './components/navbar/navbar.js';
 
 const app = document.querySelector('app');
 const page = await MainPage();
